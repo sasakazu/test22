32476056fbe2f61eeb0edd06dffbe43443ca6fe0
kazuyasasakura <kazuya.sasakura@gmial.com>
2019-05-15 13:25:44 +0900

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by 笹倉一也 on 2019/05/15.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
